1ff221fabd27d406fad6441d272d8177110df445
Harsh Deepanshu <harshdeepanshustrix@gmail.com>
2024-01-15 06:42:58 +0530

    login and signup

--- VCare-Client/src/pages/Login_Signup.jsx
+++ VCare-Client/src/pages/Login_Signup.jsx
@@ -1,7 +1,8 @@
-import { useContext, useEffect, useState } from 'react';
+
+import { useContext, useEffect, useState, Fragment } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../App';
-import './css/form.css';
+// import './css/form.css';
 
 function authenticateUser(email, password, set) {
   const endpoint = 'http://localhost:3000/login';
@@ -13,48 +14,48 @@ function authenticateUser(email, password, set) {
     },
     body: JSON.stringify({ email, password }),
   })
-  .then(response => response.json())
-  .then(data => {
-    if (data.success) {
-      set(data.success)
-    } else {
-      alert('Authentication failed. User not found!');
-    }
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    alert('An error occurred. Please try again.');
-  });
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        set(data.success)
+      } else {
+        alert('Authentication failed. User not found!');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('An error occurred. Please try again.');
+    });
 }
 
 function registerUser(name, email, password) {
   console.log(name, email, password);
-const endpoint = 'http://localhost:3000/register';
+  const endpoint = 'http://localhost:3000/register';
 
-fetch(endpoint, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ name, email, password }),
-})
-.then(response => response.json())
-.then(data => {
-  if (data.success) {
-    alert('User registered successfully. You can now log in.');
-  } else {
-    alert('Registration failed. ' + data.message);
-  }
-})
-.catch(error => {
-  console.error('Error:', error);
-  alert('An error occurred. Please try again.');
-});
+  fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name, email, password }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        alert('User registered successfully. You can now log in.');
+      } else {
+        alert('Registration failed. ' + data.message);
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('An error occurred. Please try again.');
+    });
 }
 
 const Login_Signup = () => {
   const [isRegistered, setRegistered] = useState(false);
-  const [isLoggedIn , setIsLoggedIn] = useContext(AuthContext);
+  const [isLoggedIn, setIsLoggedIn] = useContext(AuthContext);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -72,56 +73,101 @@ const Login_Signup = () => {
     registerUser(name, email, password);
   }
 
-  function toggleSignup ( ) {
+  function toggleSignup() {
     setRegistered(!isRegistered)
   }
 
-  useEffect(()=>{
-    if (isLoggedIn){
+  useEffect(() => {
+    if (isLoggedIn) {
       console.log(isLoggedIn);
       navigate('/');
     }
-  },[isLoggedIn, navigate]);
+  }, [isLoggedIn, navigate]);
 
-	return (
-		<div className='body'>
-      <h2 className='text-[#4FA097] text-[32px]' >VCARE LABS <p className='#333'>Your Wellness, Our Priority: Delivering Health to Your Doorstep!Possibilities</p></h2>
-      <div className={`container ${isRegistered? "right-panel-active": ""}`} id="container">
-        <div className="form-container sign-up-container">
-          <form id="signUpForm" action="">
-            <h1>Create Account</h1>
-            <input type="text" id="signUpName" onChange={(e)=>{setName(e.target.value)}} placeholder="Name" />
-            <input type="email" id="signUpEmail" onChange={(e)=>{setEmail(e.target.value)}} placeholder="Email" />
-            <input type="password" id="signUpPassword" onChange={(e)=>{setPassword(e.target.value)}} placeholder="Password" />
-            <button onClick={handleSignup}>Sign Up</button>
-          </form>
-        </div>
-        <div className="form-container sign-in-container">
-          <form id="signInForm" action="#">
-            <h1>Sign in</h1>
-            <input type="email" id="signInEmail" onChange={(e)=>{setUserEmail(e.target.value)}} placeholder="Email" />
-            <input type="password" id="signInPassword" onChange={(e)=>{setUserPassword(e.target.value)}} placeholder="Password" />
-            <a href="#">Forgot your password?</a>
-            <button onClick={handleLogin}>Sign In</button>
-          </form>
-        </div>
-        <div className="overlay-container">
-          <div className="overlay">
-            <div className="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
-              <p>To keep connected with us please login with your personal info</p>
-              <button className="ghost" onClick={toggleSignup} id="signIn">Sign In</button>
-            </div>
-            <div className="overlay-panel overlay-right">
-              <h1>Hello, Explorer!</h1>
-              <p>Enter your personal details and start journey with us</p>
-              <button className="ghost" onClick={toggleSignup} id="signUp">Sign Up</button>
+  return (
+    <Fragment>
+
+      {/* <section className="bg-[#4FA097] ">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+          <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-white ">
+            <img className="w-8 h-8 mr-2" src="img/logo.png" alt="logo" />
+            VCare Groups
+          </a>
+          <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-[#4FA097] md:text-2xl ">
+                Sign in to your account
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label for="email" className="block mb-2 text-sm font-medium text-[#4FA097] ">Your email</label>
+                  <input type="email" id="signUpEmail" onChange={(e)=>{setEmail(e.target.value)}} name="email" className="bg-gray-50 border border-gray-300 text-[#4FA097] sm:text-sm rounded-lg focus:ring-[#4FA097] block w-full p-2.5 " placeholder="name@company.com" required="" />
+                </div>
+                <div>
+                  <label for="password" className="block mb-2 text-sm font-medium text-[#4FA097] ">Password</label>
+                  <input type="password" id="signUpPassword" onChange={(e)=>{setPassword(e.target.value)}}  name="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-[#4FA097] sm:text-sm rounded-lg focus:ring-[#4FA097] block w-full p-2.5 " required="" />
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 " required="" />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label for="remember" className="text-gray-500">Remember me</label>
+                    </div>
+                  </div>
+                  <a href="#" className="text-sm font-medium text-primary-600 hover:underline">Forgot password?</a>
+                </div>
+                <button  type="submit" className="w-full text-white bg-[#4FA097] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"onClick={handleLogin}>Sign in</button>
+                <p className="text-sm font-light text-gray-500 ">
+                  Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline ">Sign up</a>
+                </p>
+              </form>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-	);
+      </section> */}
+
+
+
+      {/* <section className="bg-[#4FA097] ">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+          <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-white ">
+            <img className="w-8 h-8 mr-2" src="img/logo.png" alt="logo" />
+            VCare Groups
+          </a>
+          <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-[#4FA097] md:text-2xl ">
+              Create Account
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+              <div>
+                  <label for="name" className="block mb-2 text-sm font-medium text-[#4FA097] ">Your Name</label>
+                  <input type="text" id="signName" onChange={(e)=>{setName(e.target.value)}} name="name" className="bg-gray-50 border border-gray-300 text-[#4FA097] sm:text-sm rounded-lg focus:ring-[#4FA097] block w-full p-2.5 " placeholder="name@company.com" required="" />
+                </div>
+                <div>
+                  <label for="email" className="block mb-2 text-sm font-medium text-[#4FA097] ">Your email</label>
+                  <input type="email" id="signEmail" onChange={(e)=>{setEmail(e.target.value)}}  name="email" className="bg-gray-50 border border-gray-300 text-[#4FA097] sm:text-sm rounded-lg focus:ring-[#4FA097] block w-full p-2.5 " placeholder="name@company.com" required="" />
+                </div>
+                <div>
+                  <label for="password" className="block mb-2 text-sm font-medium text-[#4FA097] ">Password</label>
+                  <input type="password" id="signPassword" name="password" onChange={(e)=>{setPassword(e.target.value)}} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-[#4FA097] sm:text-sm rounded-lg focus:ring-[#4FA097] block w-full p-2.5 " required="" />
+                </div>
+               
+                <button type="submit" className="w-full text-white bg-[#4FA097] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center " onClick={handleSignup} id="signUp">Sign Up</button>
+                <p className="text-sm font-light text-gray-500 ">
+                  Already have a account? <a href="#" className="font-medium text-primary-600 hover:underline ">Log in</a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>  */}
+
+    </Fragment>
+
+  );
 };
 
 export default Login_Signup;
